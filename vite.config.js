ec5247f92a478d09b164f4872f733a253cec8884
Rayyan Zafier Leksono <140869637+Swook1@users.noreply.github.com>
2025-06-19 16:31:39 +0700

    Setup Github Actions

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Web-Portofolio/', // Nama repository Anda
+  base: '/Web-Portfolio/', 
   build: {
     outDir: 'dist'
   }
